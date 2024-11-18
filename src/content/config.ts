3731025bed3a8840f085bccda542c58703e46cd1
Fahridanaa <fahridanaaa@gmail.com>
2024-11-19 00:21:24 +0700

    chore: remove comment

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,19 +1,5 @@
 import { defineCollection, z } from "astro:content";
 
-// const comicCollection = defineCollection({
-//     type: "data",
-//     schema: z.object({
-//         title: z.string(),
-//         author: z.string(),
-//         type: z.string().transform(str => str.toLowerCase()).pipe(z.enum(["manga", "manhwa", "manhua"])),
-//         release: z.coerce.date(),
-//         updatedOn: z.coerce.date(),
-//         status: z.string().transform(str => str.toLowerCase()).pipe(z.enum(["ongoing", "completed"])),
-//         genres: z.array(z.string()),
-//         synopsis: z.string(),
-//     }),
-// });
-
 const projectCollection = defineCollection({
     schema: z.object({
         title: z.string(),
@@ -29,5 +15,4 @@ const projectCollection = defineCollection({
 
 export const collections = {
     projects: projectCollection,
-    // comics: comicCollection,
 };
