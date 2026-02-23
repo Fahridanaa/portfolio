d3db040bf928fd5b57b129ca954f377617232e41
Fahridanaa <fahridanaaa@gmail.com>
2026-02-23 20:59:56 +0700

    feat: change about me layout a little bit

--- src/components/ui/ProjectButton.tsx
+++ src/components/ui/ProjectButton.tsx
@@ -2,38 +2,38 @@ import React from "react";
 import type { LucideProps } from "lucide-react";
 
 interface ProjectButtonProps {
-	href: string;
-	icon?: React.ForwardRefExoticComponent<
-		Omit<LucideProps, "ref"> & React.RefAttributes<SVGSVGElement>
-	>;
-	text: string;
-	isNewTab?: boolean;
+  href: string;
+  icon?: React.ForwardRefExoticComponent<
+    Omit<LucideProps, "ref"> & React.RefAttributes<SVGSVGElement>
+  >;
+  text: string;
+  isNewTab?: boolean;
 }
 
 const ProjectButton: React.FC<ProjectButtonProps> = ({
-	href,
-	icon: Icon,
-	text,
-	isNewTab = true,
+  href,
+  icon: Icon,
+  text,
+  isNewTab = true,
 }) => {
-	return (
-		<a
-			className="flex justify-center items-center"
-			href={href}
-			target={isNewTab ? "_blank" : ""}
-			rel={isNewTab ? "noopener noreferrer" : undefined}
-		>
-			<button className="flex items-center justify-center font-semibold px-2 py-1 gap-2 border-1 border-[#8d7e61] bg-[#8d7e61] rounded text-[#ffffff] transition-all duration-300 hover:text-white hover:scale-105 hover:shadow-lg">
-				{Icon && (
-					<Icon
-						size={24}
-						className="transition-transform duration-300 hover:rotate-12"
-					/>
-				)}
-				{text}
-			</button>
-		</a>
-	);
+  return (
+    <a
+      className="flex justify-center items-center"
+      href={href}
+      target={isNewTab ? "_blank" : ""}
+      rel={isNewTab ? "noopener noreferrer" : undefined}
+    >
+      <button className="flex items-center justify-center font-semibold px-2 py-1 gap-2 border-1 border-[#8d7e61] bg-[#8d7e61] rounded text-[#ffffff] transition-all duration-300 hover:text-white hover:scale-105 hover:shadow-lg">
+        {Icon && (
+          <Icon
+            size={24}
+            className="transition-transform duration-300 hover:rotate-12"
+          />
+        )}
+        {text}
+      </button>
+    </a>
+  );
 };
 
 export default ProjectButton;
