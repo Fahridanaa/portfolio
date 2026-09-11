be943537bd6bfde04674316aba24511577643a12
Fahridanaa <fahridanaaa@gmail.com>
2026-09-11 13:43:27 +0700

    feat: add sitemap and robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,12 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import mdx from "@astrojs/mdx";
 import tailwindcss from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://fahridanaa.my.id',
-    integrations: [react(), mdx()],
+    integrations: [react(), mdx(), sitemap()],
     vite: {
         plugins: [tailwindcss()],
     },
